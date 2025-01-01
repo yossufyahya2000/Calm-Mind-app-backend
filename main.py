ce245c6e7f372935e50d1ea5d75de30e101814dd
yossufyahia <50063370+yossufyahya2000@users.noreply.github.com>
2025-01-01 04:13:52 +0200

    .

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-from fastapi import FastAPI, HTTPException
-from fastapi.responses import HTMLResponse
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI
 from typing import Dict
 from dotenv import load_dotenv
 import logging
-from pathlib import Path
 from app.chat.chat import ChatService
 from app.chat.message import conversationCreate
 
