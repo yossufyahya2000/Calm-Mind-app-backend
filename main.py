a5cda86765078d69b290fb7f8c70a72718e1f17b
yossufyahia <50063370+yossufyahya2000@users.noreply.github.com>
2025-01-19 17:57:31 +0200

    .

--- main.py
+++ main.py
@@ -108,7 +108,7 @@ async def create_payment_session(request: CreatePaymentSessionRequest):
         raise HTTPException(status_code=500, detail=str(e))
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, proxy_headers=True)
     
     
     
